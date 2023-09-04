e695e1b427c195a279d2173f6a408f92660c384e
Kyle Andrews <codingwithkyle@gmail.com>
2023-09-04 08:45:06 -0400

    ✨ Bootstrapped Go Fiber server

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/template/html/v2"
+)
+
+func main() {
+    // Create a new engine
+    engine := html.New("./views", ".html")
+
+    // Pass the engine to the Views
+    app := fiber.New(fiber.Config{
+        Views: engine,
+    })
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        // Render index within layouts/main
+        return c.Render("index", fiber.Map{
+            "Title": "Hello, World!",
+        })
+    })
+
+    app.Listen(":3000")
+}
