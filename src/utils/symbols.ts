cdbd82d5acee55530444f334ecc4c3f5bf8420b6
Kyle Andrews <codingwithkyle@gmail.com>
2023-09-04 08:36:30 -0400

    🔥 Old project file purge

--- src/utils/symbols.ts
+++
@@ -1,9 +0,0 @@
-export function parse(str:string): string{
-    const segments = str.match(/\{.*?\}/g);
-    for (let i = 0; i < segments.length; i++){
-        const symbol = segments[i].replace(/\{|\}/g, "");
-        const url = `https://divinedrop.nyc3.cdn.digitaloceanspaces.com/symbols/${symbol}.svg`;
-        str = str.replace(segments[i], `<img src=\"${url}\">`);
-    }
-    return str;
-}
