d19a269088b276f4df2569c50beaf78456e61e25
Kyle Andrews <codingwithkyle@gmail.com>
2023-09-04 12:52:38 -0400

    ✨ Added initial Card Browser page & search logic

---
+++ models/card.go
@@ -0,0 +1,37 @@
+package models
+
+type Card struct {
+    Id            string `gorm:"column:id;primary_key;type:binary(16)"`
+    Layout        string `gorm:"column:layout"`
+    Front         string `gorm:"column:front"`
+    Back          string `gorm:"column:back"`
+    Type          string `gorm:"column:type"`
+    Toughness     int    `gorm:"column:toughness"`
+    Power         int    `gorm:"column:power"`
+    TotalManaCost int    `gorm:"column:totalManaCost"`
+    Art           string `gorm:"column:art"`
+    Standard      bool   `gorm:"column:standard"`
+    Future        bool   `gorm:"column:future"`
+    Historic      bool   `gorm:"column:historic"`
+    Gladiator     bool   `gorm:"column:gladiator"`
+    Pioneer       bool   `gorm:"column:pioneer"`
+    Explorer      bool   `gorm:"column:explorer"`
+    Modern        bool   `gorm:"column:modern"`
+    Legacy        bool   `gorm:"column:legacy"`
+    Pauper        bool   `gorm:"column:pauper"`
+    Vintage       bool   `gorm:"column:vintage"`
+    Penny         bool   `gorm:"column:penny"`
+    Commander     bool   `gorm:"column:commander"`
+    Oathbreaker   bool   `gorm:"column:oathbreaker"`
+    Brawl         bool   `gorm:"column:brawl"`
+    HistoricBrawl bool   `gorm:"column:historicbrawl"`
+    Alchemy       bool   `gorm:"column:alchemy"`
+    PauperCommander bool `gorm:"column:paupercommander"`
+    Duel          bool   `gorm:"column:duel"`
+    OldSchool     bool   `gorm:"column:oldschool"`
+    Premodern     bool   `gorm:"column:premodern"`
+    Predh         bool   `gorm:"column:predh"`
+    Rarity        uint8  `gorm:"column:rarity"`
+    ManaCost      string `gorm:"column:manaCost"`
+}
+
--- server.go
+++ server.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+    "log"
+    "os"
     "github.com/gofiber/fiber/v2"
     "github.com/gofiber/template/html/v2"
+    "github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+    "app/models"
 )
 
+func connectDB() *gorm.DB {
+    db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to PlanetScale: %v", err)
+	}
+    return db;
+}
+
 func main() {
+    // Load environment variables from file.
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+
     // Create a new engine
     engine := html.New("./views", ".html")
 
@@ -19,10 +40,34 @@ func main() {
     app.Static("/fonts", "./public/fonts")
 
     app.Get("/", func(c *fiber.Ctx) error {
-        // Render index within layouts/main
-        return c.Render("index", fiber.Map{
-            "Title": "Hello, World!",
+        var cards []models.Card
+        db := connectDB()
+        db.Raw("SELECT front FROM Cards LIMIT 20").Scan(&cards)
+
+        return c.Render("pages/card-browser/index", fiber.Map{
+            "Page": "card-browser",
+            "Cards": cards,
+        }, "layouts/main")
+    })
+    app.Post("/partials/card-browser", func(c *fiber.Ctx) error {
+        search := c.FormValue("search")
+
+        search = "%" + search + "%"
+
+        var cards []models.Card
+        db := connectDB()
+        db.Raw("SELECT C.front FROM Cards AS C JOIN Card_Names AS CN ON C.id = CN.card_id WHERE CN.name LIKE ? LIMIT 20", search).Scan(&cards)
+
+        return c.Render("partials/card-browser", fiber.Map{
+            "Cards": cards,
         })
+    })
+
+    app.Get("/partials/nav/decks-opened", func(c *fiber.Ctx) error {
+        return c.Render("partials/nav/decks-opened", fiber.Map{})
+    })
+    app.Get("/partials/nav/decks-closed", func(c *fiber.Ctx) error {
+        return c.Render("partials/nav/decks-closed", fiber.Map{})
     })
 
     app.Listen(":3000")
