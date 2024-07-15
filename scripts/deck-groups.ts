e21ef1d5cc821713ae4b7db629ff95ccf17e95b1
Kyle Andrews <codingwithkyle@gmail.com>
2024-07-15 18:21:33 -0400

    Fixed nav deck sorting on chrome

--- scripts/deck-groups.ts
+++ scripts/deck-groups.ts
@@ -1,129 +1,49 @@
+import Sortable from 'sortablejs';
+
 declare const htmx:any;
 
 class GroupedDeck extends HTMLElement {
 
-    private dragging: boolean;
-
     constructor() {
         super();
-         this.dragging = false
     }
 
     connectedCallback() {
-        this.addEventListener("dragover", this.onDragEnter);
-        this.addEventListener("dragleave", this.onDragLeave);
-        this.addEventListener("drop", this.onDrop, { capture: true });
-        this.addEventListener("dragend", this.onDragEnd);
-        this.addEventListener("dragstart", this.onDragStart);
-
-        this.querySelectorAll(".deck-link").forEach(el => {
-            el.addEventListener("dragstart", (e: DragEvent) => {
-                this.dragging = true;
-                const target = e.currentTarget as HTMLElement;
-                e.dataTransfer.setData('text/plain', target.dataset.id);
-            });
+        const el = this.querySelector('div[x-show]');
+        new Sortable(el, {
+            group: 'decks',
+            sort: false,
+            animation: 150,
+            onEnd: (e) => {
+                if (!e?.pullMode) return;
+                const deck = e.item;
+                const activeDeckId = document.body.querySelector<HTMLInputElement>('[name="active-deck-id"]')?.value ?? "";
+                htmx.ajax('DELETE', `/decks/${deck.dataset.id}/group?active-deck-id=${activeDeckId}`, { target: '#decks', swap: 'none' });
+            }
         });
-    }
-
-    onDragStart:EventListener = (e) => {
-        this.querySelectorAll(".deck-link").forEach((el:HTMLElement) => {
-            el.classList.add("disabled");
-        });
-    }
-
-    onDragEnd:EventListener = (e) => {
-        e.preventDefault();
-        this.classList.remove("dropzone");
-        this.querySelectorAll(".deck-link").forEach((el:HTMLElement) => {
-            el.classList.remove("disabled");
-        });
-    }
-
-    onDrop:EventListener = (e:DragEvent) => {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        if (this.dragging) {
-            this.dragging = false;
-            return;
-        }
-        this.dragging = false;
-        const id = e.dataTransfer.getData('text/plain');
-        const activeDeckId = document.body.querySelector<HTMLInputElement>('[name="active-deck-id"]')?.value ?? "";
-        htmx.ajax('PUT', `/groups/${this.dataset.id}/${id}?active-deck-id=${activeDeckId}`, { target: '#decks', swap: 'innerHTML' });
-    }
-
-    onDragEnter:EventListener = (e) => {
-        e.preventDefault();
-        this.classList.add("dropzone");
-    }
-
-    onDragLeave:EventListener = (e) => {
-        e.preventDefault();
-        this.classList.remove("dropzone");
     }
 }
 if (!customElements.get("deck-group")) customElements.define("deck-group", GroupedDeck);
 
 class UngroupedDeck extends HTMLElement {
 
-    private dragging:boolean;
-
     constructor() {
         super();
-        this.dragging = false;
     }
 
     connectedCallback() {
-        this.addEventListener("dragover", this.onDragEnter);
-        this.addEventListener("dragleave", this.onDragLeave);
-        this.addEventListener("drop", this.onDrop, { capture: true });
-        this.addEventListener("dragend", this.onDragEnd);
-        this.addEventListener("dragstart", this.onDragStart);
-
-        this.querySelectorAll(".deck-link").forEach(el => {
-            el.addEventListener("dragstart", (e: DragEvent) => {
-                this.dragging = true;
-                const target = e.currentTarget as HTMLElement;
-                e.dataTransfer.setData('text/plain', target.dataset.id);
-            });
+        new Sortable(this, {
+            group: 'decks',
+            sort: false,
+            animation: 150,
+            onEnd: (e) => {
+                if (!e?.pullMode) return;
+                const deck = e.item;
+                const group = e.to.closest('deck-group');
+                const activeDeckId = document.body.querySelector<HTMLInputElement>('[name="active-deck-id"]')?.value ?? "";
+                htmx.ajax('PUT', `/groups/${group.dataset.id}/${deck.dataset.id}?active-deck-id=${activeDeckId}`, { target: '#decks', swap: 'none' });
+            }
         });
-    }
-
-    onDragStart:EventListener = (e) => {
-        this.querySelectorAll(".deck-link").forEach((el:HTMLElement) => {
-            el.classList.add("disabled");
-        });
-    }
-
-    onDragEnd:EventListener = (e) => {
-        e.preventDefault();
-        this.classList.remove("dropzone");
-        this.querySelectorAll(".deck-link").forEach((el:HTMLElement) => {
-            el.classList.remove("disabled");
-        });
-    }
-
-    onDrop:EventListener = (e:DragEvent) => {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        if (this.dragging) {
-            this.dragging = false;
-            return;
-        }
-        this.dragging = false;
-        const id = e.dataTransfer.getData('text/plain');
-        const activeDeckId = document.body.querySelector<HTMLInputElement>('[name="active-deck-id"]')?.value ?? "";
-        htmx.ajax('DELETE', `/decks/${id}/group?active-deck-id=${activeDeckId}`, { target: '#decks', swap: 'innerHTML' });
-    }
-
-    onDragEnter:EventListener = (e) => {
-        e.preventDefault();
-        this.classList.add("dropzone");
-    }
-
-    onDragLeave:EventListener = (e) => {
-        e.preventDefault();
-        this.classList.remove("dropzone");
     }
 }
 if (!customElements.get("ungrouped-decks")) customElements.define("ungrouped-decks", UngroupedDeck);
